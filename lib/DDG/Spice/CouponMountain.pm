3708051392b9c44eea100ea028c651e26b3a43f9
Mosico <qiwei.mo@gmail.com>
2013-07-24 10:58:32 +0800

    coupon search key and css modify

--- lib/DDG/Spice/CouponMountain.pm
+++ lib/DDG/Spice/CouponMountain.pm
@@ -3,30 +3,29 @@ package DDG::Spice::CouponMountain;
 use DDG::Spice;
 
 name "CouponMountain";
-description "Get the newest code for you";
+description "Get the newest coupon codes for you";
 source "coupon";
-primary_example_queries "coupon";
-secondary_example_queries "coupon dell";
-category "bang";
-topics "everyday", "food_and_drink";
+primary_example_queries "hp coupon";
+secondary_example_queries "hp promo codes";
+category "finance";
+topics "economy_and_finance", "food_and_drink", "everyday", "travel";
 icon_url "/i/couponmountain.com.ico";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/CouponMountain.pm";
 attribution github => ["https://github.com/mosico", "Mosico"],
             twitter => ["https://twitter.com/sicomo", "Mosico"];
 
-triggers end => "coupon code", "coupon codes";
-triggers any => "coupon", "coupons";
+triggers any => "coupon", "coupons", "promo code", "promo codes", "discount code", "discount codes", "promotional code", "promotional codes";
 
-spice from => '([^/]+)/?([^/]+)?';
-spice to => 'http://mamsweb101.dev.wl.mezimedia.com:8081/Coupon/searchEngine.html?keyword=$1&tabUrl=$2';
+#spice from => '([^/]+)/?([^/]+)?';
+spice to => 'http://mamsweb101.dev.wl.mezimedia.com:8081/Coupon/searchEngine.html?keyword=$1';
 spice wrap_jsonp_callback => 1;
 
 spice is_cached => 0;
 
 handle query => sub {
 	return unless (length $_);
-	my $refer = $ENV{HTTP_REFERER};
-	return $_, $refer;
+	#my $refer = $ENV{HTTP_REFERER};
+	return $_;
 };
 
 1;
--- share/spice/coupon_mountain/coupon_mountain.js
+++ share/spice/coupon_mountain/coupon_mountain.js
@@ -3,9 +3,8 @@ function ddg_spice_coupon_mountain (api_result) {
 
     if (api_result.count < 1) return;
 
-    //var q = api_result.q.replace(/\s/g, '+');
-    //var relevants = getRelevant(api_result.results);
-    var relevants = api_result.coupon;
+    var relevants = getRelevant(api_result.coupon);
+	var hander = api_result.keyword ? api_result.keyword + " (CouponMountain)" : "Coupon Search (CouponMountain)";
 
     if (!relevants) return;
 
@@ -13,32 +12,24 @@ function ddg_spice_coupon_mountain (api_result) {
         data: api_result,
         source_name: 'CouponMountain',
         source_url: 'http://www.couponmountain.com',
-        header1: 'Coupons (CMUS)',
+        header1: hander,
         //force_big_header: true,
         //more_logo: "quixey_logo.png",
         template_frame: "carousel",
         template_normal: "coupon_mountain",
+		template_options: {li_width: 150, li_height: 138},
         carousel_css_id: "coupon_mountain",
         carousel_template_detail: "coupon_mountain_detail",
         carousel_items: relevants
     });
-}
 
-var isReset = false;
-function resetLiSize() {
-	if (isReset) return;
-	var liHandle = $("#ddgc_slides li");
-	if (liHandle.length > 0) {
-		$("#ddgc_slider").css("height", "140px");
-		$("#ddgc_frame").css("height", "140px");
-		$("#ddgc_slides").css("height", "140px");
-		$("#ddgc_slides li").css({width: "160px", height: "138px"});
-		isReset = true;
-	} else {
-		setTimeout(resetLiSize, 200);
+	function getRelevant (coupons) {
+		for (var i = 0; i < coupons.length; i++) {
+			if (coupons[i].expire == "3333-03-03") {
+				coupons[i].expire = "";
+			}
+		}
+		return coupons;
 	}
 }
 
-$(document).ready(function(){
-	resetLiSize();
-});
